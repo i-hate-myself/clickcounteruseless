ef158000e22e328fd295b2f058d78a3cd4e0026d
i-hate-myself <andioof@protonmail.com>
2020-04-22 16:32:48 +1000

    added it all

---
+++ clickcout/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clickcout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int clicks = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clicks += 1;
+            label1.Text = Convert.ToString(clicks);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clicks = 0;
+            label1.Text = Convert.ToString(clicks);
+        }
+    }
+}
